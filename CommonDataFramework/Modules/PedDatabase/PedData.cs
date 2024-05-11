c40868fd1c8ba65e3459eaa9b95fa5772e2a0ac1
Sprayxe <sprayxe.marcel@gmail.com>
2024-05-11 15:31:47 +0200

    personaaaaaaaaa

--- CommonDataFramework/Modules/PedDatabase/PedData.cs
+++ CommonDataFramework/Modules/PedDatabase/PedData.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using CommonDataFramework.Utility.Extensions;
 using LSPD_First_Response.Engine.Scripting.Entities;
+using static CommonDataFramework.Utility.Helpers.DateTimeHelper;
 
 namespace CommonDataFramework.Modules.PedDatabase;
 
@@ -8,26 +10,31 @@ namespace CommonDataFramework.Modules.PedDatabase;
 /// </summary>
 public class PedData
 {
-    internal Ped Holder { get; private set; }
-    internal Persona Persona { get; private set; }
+    internal readonly Ped Holder;
     
+    /// <summary>
+    /// The persona record of the ped.
+    /// </summary>
+    /// <seealso cref="Persona"/>
+    public readonly Persona Record;
+
     /// <summary>
     /// The hunting permit of the ped.
     /// </summary>
     /// <seealso cref="Permit"/>
-    public Permit HuntingPermit { get; private set; }
+    public readonly Permit HuntingPermit;
     
     /// <summary>
     /// The fishing permit of the ped.
     /// </summary>
     /// <seealso cref="Permit"/>
-    public Permit FishingPermit { get; private set; }
+    public readonly Permit FishingPermit;
     
     /// <summary>
     /// The weapon permit of the ped.
     /// </summary>
     /// <seealso cref="WeaponPermit"/>
-    public WeaponPermit WeaponPermit { get; private set; }
+    public readonly WeaponPermit WeaponPermit;
     
     /// <summary>
     /// The expiration date of the ped's drivers license (if the ped owns one).
@@ -38,10 +45,41 @@ public class PedData
     internal PedData(Ped holder)
     {
         Holder = holder;
-        Persona = LSPDFRFunctions.GetPersonaForPed(holder); // TODO clone?
+        Record = LSPDFRFunctions.GetPersonaForPed(holder).Clone();
         HuntingPermit = new Permit();
         FishingPermit = new Permit();
         WeaponPermit = new WeaponPermit();
-        // TODO DriversLicenseExpiration
+        HandleDriversLicenseExpiration();
+    }
+
+    /// <summary>
+    /// Changes the status of the ped's license and updates the expiration date of the license accordingly.
+    /// </summary>
+    /// <param name="licenseState">The new status of the ped's license.</param>
+    /// <seealso cref="ELicenseState"/>
+    /// <seealso cref="DriversLicenseExpiration"/>
+    public void SetDriversLicenseState(ELicenseState licenseState)
+    {
+        Record.ELicenseState = licenseState;
+        HandleDriversLicenseExpiration();
+    }
+
+    private void HandleDriversLicenseExpiration()
+    {
+        switch (Record.ELicenseState)
+        {
+            case ELicenseState.Suspended:
+            case ELicenseState.Expired:
+                DriversLicenseExpiration = GetRandomDateTimeWithinRange(4);
+                break;
+            case ELicenseState.Valid:
+                DriversLicenseExpiration = GetRandomDateTimeWithinRange(CurrentDate.AddYears(4));
+                break;
+            case ELicenseState.None:
+            case ELicenseState.Unlicensed:
+            default:
+                DriversLicenseExpiration = null;
+                break;
+        }
     }
 }
---
+++ CommonDataFramework/Utility/Extensions/PersonaExtensions.cs
@@ -0,0 +1,27 @@
+﻿using LSPD_First_Response.Engine.Scripting.Entities;
+
+namespace CommonDataFramework.Utility.Extensions;
+
+internal static class PersonaExtensions
+{
+    internal static Persona Clone(this Persona persona) =>
+        new(persona.Forename, persona.Surname, persona.Gender, persona.Birthday)
+        {
+            AdvisoryText = persona.AdvisoryText,
+            Citations = persona.Citations,
+            ELicenseState = persona.ELicenseState,
+            RuntimeInfo = persona.RuntimeInfo,
+            TimesStopped = persona.TimesStopped,
+            Wanted = persona.Wanted,
+            WantedInformation =
+            {
+                CanBeSearchedInManhunt = persona.WantedInformation.CanBeSearchedInManhunt,
+                CarriesEvidence = persona.WantedInformation.CarriesEvidence,
+                EscapedInVehicle = persona.WantedInformation.EscapedInVehicle,
+                GetawayCar = persona.WantedInformation.GetawayCar,
+                IsWantedInManhunt = persona.WantedInformation.IsWantedInManhunt,
+                LastSeenPosition = persona.WantedInformation.LastSeenPosition,
+                LastSeenUtc = persona.WantedInformation.LastSeenUtc
+            }
+        };
+}
