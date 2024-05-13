d81fc96aafe1028fa1255bbf2e5d9e8ca30bd162
Sprayxe <sprayxe.marcel@gmail.com>
2024-05-13 21:30:52 +0200

    PedRecord

--- CommonDataFramework/Modules/PedDatabase/PedData.cs
+++ CommonDataFramework/Modules/PedDatabase/PedData.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using CommonDataFramework.Engine.Utility.Extensions;
-using LSPD_First_Response;
-using LSPD_First_Response.Engine.Scripting.Entities;
-using static CommonDataFramework.Engine.Utility.Helpers.DateTimeHelper;
+﻿using CommonDataFramework.Engine.Utility.Extensions;
 
 namespace CommonDataFramework.Modules.PedDatabase;
 
 /// <summary>
 /// Represents a data record of a <see cref="Rage.Ped"/>.
 /// </summary>
-public class PedData
+public class PedData : PedRecord
 {
     internal readonly Ped Holder;
-    internal readonly Persona Record;
     
     /// <summary>
     /// Once the ped that owns this ped data stops existing, the ped data is scheduled for deletion.
@@ -37,164 +32,12 @@ public class PedData
     /// </summary>
     /// <seealso cref="WeaponPermit"/>
     public readonly WeaponPermit WeaponPermit;
-    
-    /// <summary>
-    /// The expiration date of the ped's drivers license (if the ped owns one).
-    /// </summary>
-    public DateTime? DriversLicenseExpiration { get; private set; }
-    // TODO PedAddress
 
-    /// <summary>
-    /// Gets or sets the current state of the ped's drivers license.
-    /// This also affects <see cref="DriversLicenseExpiration"/>.
-    /// </summary>
-    /// <seealso cref="ELicenseState"/>
-    public ELicenseState DriversLicenseState
-    {
-        get => Record.ELicenseState;
-        set => SetDriversLicenseState(value);
-    }
-    
-    /// <summary>
-    /// Gets or sets the firstname of the ped.
-    /// </summary>
-    public string Firstname
-    {
-        get => Record.Forename;
-        set => Record.Forename = value;
-    }
-
-    /// <summary>
-    /// Gets or sets the lastname of the ped.
-    /// </summary>
-    public string Lastname
-    {
-        get => Record.Surname;
-        set => Record.Surname = value;
-    }
-    
-    /// <summary>
-    /// Returns the full name of the ped.
-    /// </summary>
-    public string FullName => Record.FullName;
-
-    /// <summary>
-    /// Gets or sets the birthday of the ped.
-    /// </summary>
-    public DateTime Birthday
-    {
-        get => Record.Birthday;
-        set => Record.Birthday = value;
-    }
-
-    // TODO 'GiveCitationToPed'
-    /// <summary>
-    /// Gets the amount of citations the ped has received.
-    /// </summary>
-    public int Citations => Record.Citations;
-    
-    /// <summary>
-    /// Gets or sets the amount of times the ped was stopped.
-    /// </summary>
-    public int TimesStopped
-    {
-        get => Record.TimesStopped;
-        set => Record.TimesStopped = value;
-    }
-
-    /// <summary>
-    /// Gets or sets the gender of the ped.
-    /// </summary>
-    public Gender Gender
-    {
-        get => Record.Gender;
-        set => Record.Gender = value;
-    }
-
-    /// <summary>
-    /// Gets or sets whether the ped is wanted.
-    /// </summary>
-    public bool Wanted
-    {
-        get => Record.Wanted;
-        set => Record.Wanted = value;
-    }
-
-    /// <summary>
-    /// Gets or sets the advisory text for this ped.
-    /// </summary>
-    public string AdvisoryText
-    {
-        get => Record.AdvisoryText;
-        set => Record.AdvisoryText = value;
-    }
-
-    /// <summary>
-    /// Gets the model age of this ped.
-    /// </summary>
-    /// <seealso cref="PedModelAge"/>
-    public PedModelAge ModelAge => Record.ModelAge;
-
-    /// <summary>
-    /// Gets or sets the runtime information for this ped.
-    /// </summary>
-    /// <seealso cref="RuntimePersonaInformation"/>
-    public RuntimePersonaInformation RuntimeInfo
-    {
-        get => Record.RuntimeInfo;
-        set => Record.RuntimeInfo = value;
-    }
-
-    /// <summary>
-    /// Gets the wanted information for this ped.
-    /// </summary>
-    /// <seealso cref="WantedInformation"/>
-    public WantedInformation WantedInfo => Record.WantedInformation;
-
-    internal PedData(Ped holder)
+    internal PedData(Ped holder) : base(LSPDFRFunctions.GetPersonaForPed(holder).Clone())
     {
         Holder = holder;
-        Record = LSPDFRFunctions.GetPersonaForPed(holder).Clone();
         HuntingPermit = new Permit();
         FishingPermit = new Permit();
         WeaponPermit = new WeaponPermit();
-        HandleDriversLicenseExpiration();
-    }
-
-    /// <summary>
-    /// Returns a string containing fullname and date of birth of the ped.
-    /// </summary>
-    /// <returns>The string with fullname and birthday combined.</returns>
-    public string ToNameAndDOBString() => Record.ToNameAndDOBString();
-
-    /// <summary>
-    /// Changes the status of the ped's license and updates the expiration date of the license accordingly.
-    /// </summary>
-    /// <param name="licenseState">The new status of the ped's license.</param>
-    /// <seealso cref="ELicenseState"/>
-    /// <seealso cref="DriversLicenseExpiration"/>
-    private void SetDriversLicenseState(ELicenseState licenseState)
-    {
-        Record.ELicenseState = licenseState;
-        HandleDriversLicenseExpiration();
-    }
-
-    private void HandleDriversLicenseExpiration()
-    {
-        switch (Record.ELicenseState)
-        {
-            case ELicenseState.Suspended:
-            case ELicenseState.Expired:
-                DriversLicenseExpiration = GetRandomDateTimeWithinRange(4);
-                break;
-            case ELicenseState.Valid:
-                DriversLicenseExpiration = GetRandomDateTimeWithinRange(CurrentDate.AddYears(4));
-                break;
-            case ELicenseState.None:
-            case ELicenseState.Unlicensed:
-            default:
-                DriversLicenseExpiration = null;
-                break;
-        }
     }
 }
