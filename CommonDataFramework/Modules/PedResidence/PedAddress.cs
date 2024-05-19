a406ca0e849efed5a22ed1bfdf359e726bcb3ba5
Echooo29 <need2speed29@gmail.com>
2024-05-18 20:45:16 -0400

    Set up address and postal code systems

---
+++ CommonDataFramework/Modules/PedResidence/PedAddress.cs
@@ -0,0 +1,28 @@
+﻿using CommonDataFramework.Modules.Postals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonDataFramework.Modules.PedResidence;
+
+public class PedAddress
+{
+
+    public static Postal AddressPostal { get; set; }
+
+    public static string StreetName { get; set; }
+
+    public PedAddress(Postal postal, string address)
+    {
+        AddressPostal = postal;
+        StreetName = address;
+    }
+
+    public PedAddress(Vector3 position)
+    {
+        AddressPostal = PostalCodeHandler.GetNearestPostalCode(position).Code;
+        StreetName = World.GetStreetName(position);
+    }
+}
---
+++ CommonDataFramework/Modules/Postals/PostalCodeHandler.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using LSPD_First_Response.Mod.API;
+using Rage;
+
+namespace CommonDataFramework.Modules.Postals;
+
+/// <summary>
+/// Handles the postal codes.
+/// </summary>
+internal static class PostalCodeHandler
+{
+    /// <summary>
+    /// Gets the active postal code set.
+    /// </summary>
+    internal static PostalCodeSet PostalCodeSet { get; private set; } = null;
+
+    /// <summary>
+    /// The path for the postal codes xml.
+    /// </summary>
+    internal static string PostalXmlPath = "";
+
+    /// <summary>
+    /// Gets the postal code as a stringed number.
+    /// </summary>
+    /// <param name="position">The position for the code.</param>
+    /// <returns>The code.</returns>
+    public static string GetPostalCode(Vector3 position)
+    {
+        if (PostalCodeSet != null)
+        {
+            var code = GetNearestPostalCode(position);
+            if (code != null)
+            {
+                return code.Code.Number;
+            }
+        }
+
+        return string.Empty;
+    }
+
+    /// <summary>
+    /// Gets the nearest postal code to a given position.
+    /// </summary>
+    /// <param name="position">The position.</param>
+    /// <returns>The nearest postal code.</returns>
+    public static NearestPostalCode GetNearestPostalCode(Vector3 position)
+    {
+        var iter = PostalCodeSet.Codes.Where(code =>
+            code.X > position.X - 500 &&
+            code.X < position.X + 500 &&
+            code.Y > position.Y - 500 &&
+            code.Y < position.Y + 500);
+
+        float nearestDistance = -1;
+        Postal nearestCode = null;
+        foreach (var entry in iter)
+        {
+            var codePosition = new Vector3(entry.X, entry.Y, 0);
+            var codeDistance = codePosition.DistanceTo(position);
+            if (nearestCode == null || codeDistance < nearestDistance)
+            {
+                nearestCode = entry;
+                nearestDistance = codeDistance;
+            }
+        }
+
+        if (nearestCode != null)
+        {
+            return new NearestPostalCode(nearestCode, nearestDistance);
+        }
+
+        return null;
+    }
+
+    private static void Load()
+    {
+        foreach (var filename in Directory.GetFiles(PostalXmlPath).Where(x => x.EndsWith(".xml")))
+        {
+            var postalCodeSet = PostalCodeSet.FromXML(filename);
+            if (postalCodeSet != null)
+            {
+                PostalCodeSet = postalCodeSet;
+            }
+        }
+    }
+}
